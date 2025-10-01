2aa3a8e69f2dd28ad6b6f0f07599080c6d56c561
Mert Buyuknisan <mbuyuknisan1@gmail.com>
2025-10-01 17:47:20 +0300

    feat:ilk iki görev eklendi

---
+++ flutterhafta2/lib/birincisayfa.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'ikincisayfa.dart';
+
+class BirinciSayfa extends StatefulWidget {
+  const BirinciSayfa({super.key});
+
+  @override
+  State<BirinciSayfa> createState() => _BirinciSayfaState();
+}
+
+class _BirinciSayfaState extends State<BirinciSayfa> {
+
+  Color _backgroundColor = Colors.blue.shade700;
+
+  void _arkaplandegistir() {
+
+    setState(() {
+      _backgroundColor = Colors.indigoAccent.shade700;
+    });
+  }
+
+  void _mesajgoster(){
+   const snackbar=SnackBar(content:Text("Merhaba Flutter!!"));
+   ScaffoldMessenger.of(context).showSnackBar(snackbar);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: _backgroundColor,
+      appBar: AppBar(
+        title: Text(
+          "Birinci Sayfa",
+        ),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              "Butonlar ile renk değiştirme ve mesaj gösterme",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Image.network(
+                  "https://cmspanel.tvplus.com.tr/uploads/marvel0_5_005fa9e70c.jpg"),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: _arkaplandegistir,
+                    child: Text(
+                      "Arka Plan Rengini Değiştir",
+                      style: TextStyle(color: Colors.black),
+                    )),
+                IconButton(
+                  onPressed: _mesajgoster,
+                  icon: Icon(Icons.message),
+                  iconSize: 30.0,
+                  color: Colors.red,
+                ),
+                ElevatedButton(onPressed:(){
+                 Navigator.push(context, MaterialPageRoute(builder: (context)=>IkinciSayfa()));
+                }, child: Text("Diğer Sayfaya Git"))
+              ],
+
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
